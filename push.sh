014319c76f35433a3fb0f1f3722d35270f682e95
Bart-Nikita <info@bart-group.com>
2025-06-02 21:07:56 +0300

    fix problems

--- push.sh
+++ push.sh
@@ -1,1 +1,4 @@
 git add . && git commit -m "$1" && git push origin main
+
+
+git remote  set-url origin https://nikita-bondarenko:https://nikita-bondarenko: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@github.com/nikita-bondarenko/cryptus_2025.git
