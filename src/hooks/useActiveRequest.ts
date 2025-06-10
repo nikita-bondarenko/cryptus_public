1b16519818a403acf47484b6422768cc7a43d9c1
Nikita Bondarenko <naradadasosmi@gmail.com>
2025-06-10 14:50:38 +0300

    fix filter undefined error

--- src/hooks/useActiveRequest.ts
+++ src/hooks/useActiveRequest.ts
@@ -4,22 +4,22 @@ import { useMemo } from "react";
 import { useQueryWithLoading } from "./useQueryWithLoading";
 import { IN_PROCESS_REQUEST_STATUS, NEW_REQUEST_STATUS } from "@/config";
 
-
-
 export const useActiveRequest = () => {
   const userId = useAppSelector((state) => state.userData.userId);
   const isAppReady = useAppSelector((state) => state.ui.isAppReady);
   const { data: exchanges, isLoading } = useGetUserExchangesQuery({ user_id: userId }, { skip: !userId || !isAppReady })
 
   const activeRequests = useMemo(() => {
-    return exchanges?.filter(
-      (exchange) => exchange.status === IN_PROCESS_REQUEST_STATUS||exchange.status === NEW_REQUEST_STATUS
+    if (!Array.isArray(exchanges)) return [];
+    
+    return exchanges.filter(
+      (exchange) => exchange.status === IN_PROCESS_REQUEST_STATUS || exchange.status === NEW_REQUEST_STATUS
     );
   }, [exchanges, isAppReady]);
 
   return {
     activeRequests,
     isLoading,
-    hasActiveRequest: !!activeRequests,
+    hasActiveRequest: Array.isArray(activeRequests) && activeRequests.length > 0,
   };
 }; 
