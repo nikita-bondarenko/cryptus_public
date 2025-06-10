1726fb6d720bc1c1d4ec2242fe911f0ffb78c8a5
Nikita Bondarenko <naradadasosmi@gmail.com>
2025-06-10 12:16:21 +0300

    set config for tg mini app

---
+++ next.config.js
@@ -0,0 +1,49 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['cryptus-2025-3f67.vercel.app'],
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://web.telegram.org https://telegram.org"
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://web.telegram.org'
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          }
+        ],
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://cryptus-2.0.1362967-ci52663.tw1.ru/:path*',
+      },
+    ]
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+}
+
+module.exports = nextConfig 
