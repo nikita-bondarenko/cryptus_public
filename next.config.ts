a41599d43261e96216578a02b1b37f4c4318e23b
Nikita Bondarenko <naradadasosmi@gmail.com>
2025-06-10 13:44:33 +0300

    update telegram rovider

--- next.config.ts
+++ next.config.ts
@@ -2,30 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['cryptus-2025.vercel.app'],
-    unoptimized: true,
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://*.telegram.org https://t.me"
+  reactStrictMode: false,
 
-          },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*'
-          }
-        ],
-      },
-    ]
-  },
+
 
 };
 
--- src/components/TelegramWebAppProvider.tsx
+++ src/components/TelegramWebAppProvider.tsx
@@ -1,49 +1,41 @@
-'use client'
+"use client";
 
-import { useEffect } from 'react'
-import Script from 'next/script'
-import { setIsAppReady } from '@/redux/slices/uiSlice'
-import { useAppDispatch } from '@/redux/hooks'
+import { useEffect } from "react";
+import Script from "next/script";
+import { setIsAppReady } from "@/redux/slices/uiSlice";
+import { useAppDispatch } from "@/redux/hooks";
 
-
-
-
-export function TelegramWebAppProvider({ children }: { children: React.ReactNode }) {
-
-    const dispatch = useAppDispatch()
+export function TelegramWebAppProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const dispatch = useAppDispatch();
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const script = document.createElement('script')
-      script.src = 'https://telegram.org/js/telegram-web-app.js'
-      script.async = true
-      script.onload = () => {
-        if (window.Telegram?.WebApp) {
-          window.Telegram.WebApp.ready()
-          window.Telegram.WebApp.expand()
-          dispatch(setIsAppReady(true))
-        }
-      }
-      document.body.appendChild(script)
+    // if (typeof window !== 'undefined') {
+    //   const script = document.createElement('script')
+    //   script.src = 'https://telegram.org/js/telegram-web-app.js'
+    //   script.async = true
+    //   script.onload = () => {
+    //     if (window.Telegram?.WebApp) {
+    //       window.Telegram.WebApp.ready()
+    //       window.Telegram.WebApp.expand()
+    //       dispatch(setIsAppReady(true))
+    //     }
+    //   }
+    //   document.body.appendChild(script)
 
-      return () => {
-        document.body.removeChild(script)
-      }
+    //   return () => {
+    //     document.body.removeChild(script)
+    //   }
+    // }
+    if (window.Telegram?.WebApp) {
+      window.Telegram.WebApp.ready();
+      window.Telegram.WebApp.expand();
+      dispatch(setIsAppReady(true));
     }
-  }, [])
+    // dispatch(setIsAppReady(true))
+  }, []);
 
-  return (
-    <>
-      <Script
-        src="https://telegram.org/js/telegram-web-app.js"
-        strategy="beforeInteractive"
-        onLoad={() => {
-          if (window.Telegram?.WebApp) {
-            window.Telegram.WebApp.ready()
-            window.Telegram.WebApp.expand()
-          }
-        }}
-      />
-      {children}
-    </>
-  )
-} 
+  return (<>{children}</>);
+}
