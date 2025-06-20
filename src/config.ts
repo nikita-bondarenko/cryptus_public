339129573628d2a92d090e675e6de486fba79205
root <root@v2815798.hosted-by-vdsina.ru>
2025-06-20 14:25:04 +0300

    update api link

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'https://cryptus-2.0.1362967-ci52663.tw1.ru'; 
+export const API_URL = 'https://cryptus-backend.partners-bot.ru'; 
 export const TEST_USER_ID = 464552386;
 export const POLICY_URL = "https://telegra.ph/POLITIKA-UPRAVLENIYA-RISKAMI-AML-01-20-2";
 export const TERMS_URL = "https://telegra.ph/Polzovatelskoe-soglashenie-telegram-bota-CryptusExchange-03-19-2";
